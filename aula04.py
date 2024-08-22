556fd4cff3d1ddc70b54931e158dabc6a0933238
JoaoPedrogui <joaopedroguilicru@gmail.com>
2024-08-22 11:33:04 -0300

    meu primeiro projrto python

---
+++ aula04.py
@@ -0,0 +1,21 @@
+nomes = ["JOAO", "PEDRO", "GUIMARAES", "LIMA", "ronaldo", "cristiano" ]
+
+# para retornar o primeiro elemento da lista
+print(nomes[0])
+
+# para retornar todos elementos da lista
+print(nomes[:])
+print(nomes)
+
+# para retornar elemento 2 ate o final da lista
+print(nomes[2:])
+
+# para retornar do primeiro elemento ate o elemento 3
+print(nomes[:3])
+
+# para retornar os elementos entre 2 e 4 (vai motrar elemento 2 e 3)
+print(nomes[2:4])
+
+# as listas em python, permite adicicior tipos de dados de diferentes
+numeros = [1,2,3,4,5,6,7,8,9, "joao", "gomes",1.5, True]
+print(numeros)
